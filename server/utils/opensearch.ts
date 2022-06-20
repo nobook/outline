3754dd7f0ed783d22b9b1e0ccef3f43826aa60c9
onlyjyf <onlyjyf@nobook.com>
2022-06-20 16:51:36 +0800

    修改图标

--- server/utils/opensearch.ts
+++ server/utils/opensearch.ts
@@ -2,7 +2,7 @@ export const opensearchResponse = (baseUrl: string): string => {
   return `
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
   <ShortName>Outline</ShortName>
-  <Description>Search Outline</Description>
+  <Description>NOBOOK虚拟实验-帮助中心</Description>
   <InputEncoding>UTF-8</InputEncoding>
   <Image width="16" height="16" type="image/x-icon">${baseUrl}/favicon.ico</Image>
   <Url type="text/html" method="get" template="${baseUrl}/search/{searchTerms}?ref=opensearch"/>
